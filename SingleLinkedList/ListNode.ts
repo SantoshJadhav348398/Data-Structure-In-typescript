449dd3842339c22a19e5adacb7a2dbbc1a43a5bc
Santosh <axelblaze348398@gmail.com>
2019-10-28 21:42:50 +0530

    printing backward and forward in same line

--- SingleLinkedList/ListNode.ts
+++ SingleLinkedList/ListNode.ts
@@ -2,11 +2,13 @@ export class ListNode
 {
     value : number;
     next : ListNode;
+    prev : ListNode;
 
     constructor(val : number)
     {
         this.value = val;
         this.next = null;
+        this.prev = null;
     }
 
 }
